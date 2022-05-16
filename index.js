3d445a42def8f6ac70c11b58ac74b31227805aea
douglasneves-net <d88.neves@gmail.com>
2022-05-16 13:54:40 -0300

    File static
    File static

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const path  = require('path');
 
 app.use(express.urlencoded({extended: true}));
 
+app.use(express.static(path.resolve(__dirname, 'public')));
+
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
