250fb73cd33b10c915fb3a529b0f727adbf7386c
douglasneves-net <d88.neves@gmail.com>
2022-05-17 14:39:16 -0300

    Adjust
    Adjust

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,7 +2,10 @@
 
 exports.paginaInicial = (req, res) => {
   console.log(req.flash('info'));
-  res.render("index");
+  res.render("index", {
+    titulo: "Titulo",
+    numeros: [0,1,2,3,4,5,6,7,8,9]
+  });
 }
 
 exports.trataPost = (req, res) => {
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,3 +1,4 @@
 exports.middlewareGlobal = (req, res, next) => {
+  res.locals.umaVariavelLocal = 'Este é o valor da variavel local'
   next();
 } 
